cbe833d45c76b768d804a91bba504e64569101f0
natebachmeier <nate@bachmeier>
2020-11-11 21:21:13 -0500

    add comments

--- EmailServiceLambda/EmailAudit.cs
+++ EmailServiceLambda/EmailAudit.cs
@@ -10,16 +10,35 @@ namespace EmailServiceLambda
     /// </summary>
     public class EmailAudit
     {
+        /// <summary>
+        /// Gets or sets the partition identifier.
+        /// </summary>
         [DynamoDBHashKey]
         public string PartitionKey { get; set; }
 
+        /// <summary>
+        /// Gets or sets the item identifier within the <see cref="PartitionKey"/>.
+        /// </summary>
         [DynamoDBRangeKey]
         public string SortKey { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Time to Live (TTL).
+        /// </summary>
         [DynamoDBProperty(StoreAsEpoch = true)]
         public DateTime Expiration { get; set; } = DateTime.UtcNow + TimeSpan.FromDays(7);
 
+        /// <summary>
+        /// An opaque string upto 400KB (- envelope) in size.
+        /// </summary>
         [DynamoDBProperty]
         public string Contents { get; set; }
+
+        /// <summary>
+        /// Do not set this property.
+        /// It is managed by <see cref="IDynamoDBContext"/>.
+        /// </summary>
+        [DynamoDBVersion]
+        public int? ETag { get; set; }
     }
 }
